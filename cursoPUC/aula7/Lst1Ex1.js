55d7c2b8437eb182096962be8809c1609dfb82b6
Raphael Ramalho <rsramalhovf@gmail.com>
2021-10-13 21:59:13 -0300

    update

--- cursoPUC/aula7/Lst1Ex1.js
+++ cursoPUC/aula7/Lst1Ex1.js
@@ -1,9 +1,42 @@
-function perimetro (lado1,lado2,lado3)
-{
-  return lado1+lado2+lado3
+function perimetro (lado1,lado2,lado3){
+    return lado1+lado2+lado3
 }
 
-let lado1=+prompt("Digite a medida do primeiro lado:","medida do primeiro lado");
-let lado2=+prompt("Digite a medida do segundo lado:","medida do segundo lado");
-let lado3=+prompt("Digite a medida do terceiro lado:","medida do terceiro lado")
-console.log("O perímetro vale "+perimetro(lado1,lado2,lado3))
+function validador(valor){
+  console.log(!isNaN(valor))
+  return !isNaN(valor)
+}
+
+function tipoDeTriangulo(lado1, lado2, lado3){
+  if(lado1 == lado2 && lado1 == lado3 && lado2==lado3){         //Triangulo equilátero
+    return "Equilátero"
+  } else if (lado1 == lado2 || lado1 == lado3 || lado2==lado3){ //Triangulo isósceles
+    return "Isósceles"
+  } else {                                                      //Triangulo escaleno
+    return "Escaleno"
+  }
+}
+
+const ladoTexto = ["primeiro", "segundo", "terceiro"]
+
+const lado = []
+
+for (i = 0; i < 3; i++){
+  let valor=+prompt(`Digite a medida do ${ladoTexto[i]} lado:`,`medida do ${ladoTexto[i]} lado`)
+
+  while (!validador(valor)) {
+    console.log(undefined)
+    valor=+prompt(`Digite a medida do ${ladoTexto[i]} lado:`,`medida do ${ladoTexto[i]} lado`)
+  }
+
+  lado.push(valor)
+}
+
+console.log("O perímetro vale "+perimetro(lado[0],lado[1],lado[2]))
+console.log("O triangulo é "+tipoDeTriangulo(lado[0],lado[1],lado[2]))
+
+
+
+// let lado1=validador(+prompt("Digite a medida do primeiro lado:","medida do primeiro lado"))
+// let lado2=validador(+prompt("Digite a medida do segundo lado:","medida do segundo lado"))
+// let lado3=validador(+prompt("Digite a medida do terceiro lado:","medida do terceiro lado"))
