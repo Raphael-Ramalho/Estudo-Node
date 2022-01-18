afe4d168e5a024bdd2dfad4012a9fd6bb9aaed71
Raphael Ramalho <rsramalhovf@gmail.com>
2022-01-18 16:22:46 -0300

    Update index.js

--- agenda/index.js
+++ agenda/index.js
@@ -2,4 +2,6 @@ const express = require('express')
 
 const app = express()
 
-app.listen(4000, () => console.log('servidor rodando na porta 3000'))
+app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+
+app.get('/atendimentos', (req, res) => res.send('Servidor rodando, tudo ok'))
