32155ed5a5eef592666d49eaa9c8fe73aca381b5
Raphael-Ramalho <rsramalhovf@gmail.com>
2022-02-13 14:30:17 -0300

    update

--- agenda/index.js
+++ agenda/index.js
@@ -1,5 +1,13 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
 
+conexao.connect((erro) =>{
+  if(erro){
+    console.log(erro)
+  } else {
+    console.log('conectado com sucesso')
+  }
+})
 const app = customExpress()
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
