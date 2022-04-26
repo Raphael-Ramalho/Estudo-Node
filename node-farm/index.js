fa3d4adafd9f8530dc9c63b35f1086296c86bf76
Raphael Ramalho <rsramalhovf@gmail.com>
2022-04-26 09:58:25 -0300

    update

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,4 +1,16 @@
 const fs = require('fs')
 
-const text = fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log('text: ', text)
+// //Blocking, Synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// const textOut = `This is what we know about the avocato ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log("File written!")
+
+//Non-blocking, Asyncronous way
+fs.readFile("./txt/start.txt","utf-8", (err, data)=>{
+    console.log(data)
+})
+console.log("Will read file!")
+
+
+
