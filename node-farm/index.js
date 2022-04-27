c23701337bef3946c472182ba5a75b37c633424b
Raphael Ramalho <rsramalhovf@gmail.com>
2022-04-27 01:18:04 -0300

    update

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,4 +1,8 @@
 const fs = require('fs')
+const http = require('http')
+
+///////////////////////////////////////
+/// FILES
 
 // //Blocking, Synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
@@ -6,11 +10,31 @@ const fs = require('fs')
 // fs.writeFileSync('./txt/output.txt', textOut)
 // console.log("File written!")
 
-//Non-blocking, Asyncronous way
-fs.readFile("./txt/start.txt","utf-8", (err, data)=>{
-    console.log(data)
+// //Non-blocking, Asyncronous way
+// fs.readFile("./txt/start.txt","utf-8", (err, data1)=>{
+//     if(err) return console.log("ERROR!")
+//     fs.readFile(`./txt/${data1}.txt`,"utf-8", (err, data2)=>{
+//         console.log(data2)
+//         fs.readFile(`./txt/append.txt`,"utf-8", (err, data3)=>{
+//             console.log(data3)
+
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err=>{
+//                 console.log("Your file has been written")
+//             })
+//         })
+//     })
+// })
+// console.log("Will read file!")
+
+
+///////////////////////////////////////
+/// SERVER
+
+const server = http.createServer((req, res)=>{
+    res.end("Hello from the server")
 })
-console.log("Will read file!")
 
-
+server.listen(8000, '127.0.0.2', ()=>{
+    console.log("Listening to requests on port 8000")
+})
 
