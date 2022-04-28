7588d4b804c0c38a8fa440267726528b297c9966
Raphael Ramalho <rsramalhovf@gmail.com>
2022-04-28 17:13:14 -0300

    update

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,5 +1,6 @@
-const fs = require('fs')
-const http = require('http')
+const fs = require("fs")
+const http = require("http")
+const url = require('url')
 
 ///////////////////////////////////////
 /// FILES
@@ -26,15 +27,32 @@ const http = require('http')
 // })
 // console.log("Will read file!")
 
-
 ///////////////////////////////////////
 /// SERVER
 
-const server = http.createServer((req, res)=>{
-    res.end("Hello from the server")
+const server = http.createServer((req, res) => {
+  const pathName = req.url
+  if(pathName === '/overview'){
+    res.end("This is the OVERVIEW")
+  } else if (pathName === '/product'){
+    res.end('This is the PRODUCT')
+  } else if (pathName === '/api'){
+
+    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8',(err,data)=>{
+      const productData = JSON.parse(data)//parse will transforme a json object into a javascript one
+      console.log(productData)
+    }) //a variável de ambiente __dirname indica o caminho absolito do diretório contendo o arquivel que está sendo executado
+
+    res.end("API")
+  } else {
+    res.writeHead(404,{
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-word'
+    })
+    res.end("<h1>Page not found!</h1>")
+  }
 })
 
-server.listen(8000, '127.0.0.2', ()=>{
-    console.log("Listening to requests on port 8000")
+server.listen(8000, "127.0.0.2", () => {
+  console.log("Listening to requests on port 8000")
 })
-
