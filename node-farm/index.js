45b9d8ac5d1443ea4068d509ad78b1d2dbae3f1b
Raphael Ramalho <rsramalhovf@gmail.com>
2022-05-05 17:32:21 -0300

    Update index.js

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,11 +1,7 @@
 const fs = require("fs")
 const http = require("http")
 const url = require("url")
-
 const slugify = require('slugify')
-
-
-
 const replaceTemplate = require('./modules/replaceTemplate')
 
 ///////////////////////////////////////
@@ -42,6 +38,9 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
 const dataObj = JSON.parse(data)
+
+const slugs = dataObj.map(el => slugify(el.productName,{lower:true}))
+console.log(slugify('Fresh Avocados', {lower:true}))
 
 const server = http.createServer((req, res) => {
 
