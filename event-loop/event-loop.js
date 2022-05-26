c7b0f118a2f365788eb7644e2dfe283e8550abd2
Raphael Ramalho <rsramalhovf@gmail.com>
2022-05-26 16:25:27 -0300

    Update event-loop.js

--- event-loop/event-loop.js
+++ event-loop/event-loop.js
@@ -1,14 +1,14 @@
 const fs = require('fs')
 
-setTimeout(()=>console.log("Timer 1 finished"), 0)
 setImmediate(()=> console.log("Immediate 1 finished"))
+setTimeout(()=>console.log("Timer 1 finished"), 0)
 
 fs.readFile('text-file.txt',()=>{
   console.log("I/0 finished")
 
   setTimeout(()=>console.log("Timer 2 finished"), 0)
   setTimeout(()=>console.log("Timer 3 finished"), 3000)
-  setTimeout(()=>console.log("Immediate 2 finished"))
+  setImmediate(()=>console.log("Immediate 2 finished"))
 
 })
 
