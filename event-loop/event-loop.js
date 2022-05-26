913cc1447a6e6987c5e8f6446bb5c67169683358
Raphael Ramalho <rsramalhovf@gmail.com>
2022-05-26 17:30:30 -0300

    update

--- event-loop/event-loop.js
+++ event-loop/event-loop.js
@@ -1,4 +1,8 @@
 const fs = require('fs')
+const crypto = require("crypto")
+
+const start = Date.now()
+process.env.UV_THREADPOOL_SIZE = 1
 
 setImmediate(()=> console.log("Immediate 1 finished"))
 setTimeout(()=>console.log("Timer 1 finished"), 0)
@@ -10,6 +14,24 @@ fs.readFile('text-file.txt',()=>{
   setTimeout(()=>console.log("Timer 3 finished"), 3000)
   setImmediate(()=>console.log("Immediate 2 finished"))
 
+  crypto.pbkdf2('password', 'saltsdfsadfasdfsdfsdf', 100000, 1024, 'sha512', ()=>{
+    console.log(Date.now() - start,"password encrypted")
+  })
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', ()=>{
+    console.log(Date.now() - start,"password encrypted")
+  })
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', ()=>{
+    console.log(Date.now() - start,"password encrypted")
+  })
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', ()=>{
+    console.log(Date.now() - start,"password encrypted")
+  })
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', ()=>{
+    console.log(Date.now() - start,"password encrypted")
+  })
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', ()=>{
+    console.log(Date.now() - start,"password encrypted")
+  })
 })
 
 console.log("Hello from the top-level code")
