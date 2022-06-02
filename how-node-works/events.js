4f76d72dc34193372300ffeea60818526c26e393
Raphael Ramalho <rsramalhovf@gmail.com>
2022-06-02 17:25:12 -0300

    update

--- how-node-works/events.js
+++ how-node-works/events.js
@@ -1,6 +1,13 @@
 const EventEmitter = require("events")
+const http = require('http')
 
 const myEmitter = new EventEmitter()
+
+class Sales extends EventEmitter{
+  constructor(){
+    super()
+  }
+}
 
 //Observers
 myEmitter.on("newSale", ()=>{
@@ -17,3 +24,25 @@ myEmitter.on('newSale', stock =>{
 
 //Event emitters
 myEmitter.emit("newSale", 9)
+
+////////////////////////////////////////////////
+
+const server = http.createServer()
+
+server.on('request', (req, res) =>{
+  console.log("Request received!")
+  res.end('Request received!')
+})
+
+server.on('request', (req, res) =>{
+  res.end('Another request!')
+})
+
+server.on('close', ()=> {
+  console.log("ServerClosed")
+})
+
+server.listen(8000, '127.0.0.1', ()=>{
+  console.log('waiting for requests...')
+})
+
