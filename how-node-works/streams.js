e23488897e2b0809da924f3a41e88420c04775b1
Raphael Ramalho <rsramalhovf@gmail.com>
2022-06-21 16:52:17 -0300

    Update streams.js

--- how-node-works/streams.js
+++ how-node-works/streams.js
@@ -9,13 +9,24 @@ server.on('request', (req, res)=>{
   // })
 
   //Solution 2 with stream
-  const readable = fs.createReadStream('test-file.txt')
-  readable.on('data', chunk => {
-    res.write(chunk)
-  })
-  readable.on('end',()=>{
-    res.end()
-  })
+  // const readable = fs.createReadStream('test-file.txt')
+  // readable.on('data', chunk => {
+  //   res.write(chunk)
+  //   console.log('ola')
+  // })
+  // readable.on('end',()=>{
+  //   res.end()
+  // })
+  // readable.on('error', err => {
+  //   console.log(err)
+  //   res.statusCode = 500
+  //   res.end('File not found!')
+  // })
+
+  //solution 3. Using pipe
+  const readable = fs.createReadStream("test-file.txt")
+  readable.pipe(res) // readableSource.pipe(WriteableDest)
+
 })
 server.listen(8000, '127.0.0.1', ()=>{
   console.log('Listening...')
