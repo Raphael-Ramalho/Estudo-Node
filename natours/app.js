da1956b9546d3a889b479b381fff9b62463f0908
Raphael De Souza Ramalho <rsramalhovf@gmail.com>
2023-12-11 18:17:07 -0300

    added env file

--- natours/app.js
+++ natours/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // Middlewares
 
 app.use(express.json());
-app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('hello from the middleware');
@@ -26,4 +31,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-module.exports = app
+module.exports = app;
