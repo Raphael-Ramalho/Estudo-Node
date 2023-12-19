deac699624696d0e815c231d26b0acad9f51044c
Raphael De Souza Ramalho <rsramalhovf@gmail.com>
2023-12-19 18:17:47 -0300

    update

--- scratch/src/app.controller.ts
+++ scratch/src/app.controller.ts
@@ -1,1 +1,14 @@
+import { Controller, Get } from "@nestjs/common";
 
+@Controller('/app')
+export class AppController {
+  @Get('/asd')
+  getRootRoute() {
+    return "Hi there!";
+  }
+
+  @Get('/bye')
+  getByeThere(){
+    return 'bye there'
+  }
+}
